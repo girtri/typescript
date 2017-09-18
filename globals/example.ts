26234317bb29f49eed3afb856ae9204c9dcf4893
gerry <girtri@libero.it>
2017-09-18 17:20:05 +0200

    pag 230 (non funziona esempio globals)

--- globals/example.ts
+++ globals/example.ts
@@ -1,7 +1,12 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+/// <reference path="./feedme" />
+import http = require( "http" );
 
-ReactDOM.render(
-	<div></div>,
-	"root"
-);
+var FeedMe = require("feedme");
+
+http.get('http://feeds.feedburner.com/TechCrunch/startups', (res) => {
+	const parser = new FeedMe(true);
+	parser.on("title", (title: string) => {
+		console.log(title);
+	});
+	res.pipe(parser);
+});
